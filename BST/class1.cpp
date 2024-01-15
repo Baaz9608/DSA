5f32abd4c3f049ec779ed550c1b453f53e605bf0
BaazShah606 <shahbazmd3002@gmail.com>
2024-01-15 08:27:04 +0530

    bst

--- BST/class1.cpp
+++ BST/class1.cpp
@@ -50,6 +50,53 @@ int minValue(Node*root, int &ans){
      
 }
 
+Node*maxValue(Node*root){
+    if(!root) return nullptr;
+    Node*temp = root;
+
+    while (temp->right)
+    {
+        temp = temp->right;
+    }
+    
+    return temp;
+}
+
+Node* deleteFromBST(Node* root, int &target){
+    // target ko search kerna hai
+    if(!root) return nullptr;
+
+    if(root->data == target){
+
+        // 4 cases
+        if(!root->left and !root->right){
+            delete root;
+            return nullptr;
+        }else if(root->left and !root->right){
+            Node*childSubtree = root->left;
+            delete root;
+            return childSubtree;
+        }else if(!root->left and root->right){
+            Node*chidlSubtree = root->right;
+            delete root;
+            return chidlSubtree;
+        }else{
+            Node*maxi = maxValue(root->left);
+            root->data = maxi->data;
+            delete maxi;
+            // return root;  galat tarika hai
+            root->left = deleteFromBST(root->left, maxi->data);
+            return root;
+        }
+    }else if(root->data > target){
+        root->left = deleteFromBST(root->left, target);
+         
+    }else{
+        root->right = deleteFromBST(root->right, target);
+         
+    }
+}
+
 int main(){
     Node* root = nullptr;
     creatingBST(root);
